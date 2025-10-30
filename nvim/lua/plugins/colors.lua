c04b7401ecd1076632ff28c74af95708ed86c1cd
raelaraujo0 <raellyedson1@gmail.com>
2025-10-30 09:51:40 -0300

    add themes

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,20 +1,37 @@
 return {
+    -- One Dark theme 
     {
         "navarasu/onedark.nvim",
+        lazy = false, 
+        priority = 1000,
         config = function()
             require('onedark').setup {
                 style = 'dark', 
             }
-            require('onedark').load()
         end
     },
 
+    -- Tokyo Night theme
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+
+    -- Dracula theme
+    {
+        "dracula/vim",
+        name = "dracula", 
+        lazy = false, 
+        priority = 1000, 
+    },
+
+    -- Lualine Configs (status bar)
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
-            theme = 'onedark', 
+            theme = 'auto', 
         }
     },
 }
-
