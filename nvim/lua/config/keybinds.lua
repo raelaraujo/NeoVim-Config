87f9c275dce7830f728625ab6dc512ffa24b36a2
raelaraujo0 <raellyedson1@gmail.com>
2025-10-31 11:25:01 -0300

    more confs

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -10,7 +10,7 @@ local themes = {
 
 local function toggle_colorscheme()
     local current_theme = vim.g.colors_name
-    local current_index = 1 -- Definimos o índice inicial como 1 (Dracula)
+    local current_index = 1
 
     for i, theme in ipairs(themes) do
         if theme == current_theme then
@@ -22,8 +22,8 @@ local function toggle_colorscheme()
     local next_index = (current_index % #themes) + 1
     local next_theme = themes[next_index]
 
-    -- Aplica o novo tema
+    -- apply a new theme
     vim.cmd.colorscheme(next_theme)
 end
 
-vim.keymap.set("n", "<leader>tt", toggle_colorscheme, { desc = "Alternar Tema de Cor" })
+vim.keymap.set("n", "<leader>tt", toggle_colorscheme, { desc = "switch colorscheme" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.scrolloff = 8
+vim.opt.undofile = true
+vim.opt.mouse = 'a'
+vim.cmd('syntax on')
+vim.opt.clipboard = 'unnamedplus'
