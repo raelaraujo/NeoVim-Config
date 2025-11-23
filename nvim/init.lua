dfae91c6f81eee07310a2aa69c2d6333f01b28d0
Rael Araujo <raellyedson1@gmail.com>
2025-11-23 11:03:26 -0300

    diagnostic in error line

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,4 +2,11 @@ require('config.options')
 require('config.keybinds')
 require('config.lazy')
 
+vim.diagnostic.config({
+    virtual_text = {
+        source = "always",
+        prefix = " > ",
+    },
+})
+
 vim.cmd.colorscheme("catppuccin")
