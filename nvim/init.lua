c21784b9575a3b30003a8893622974bd4215e59b
Rael Araujo <raellyedson1@gmail.com>
2025-11-26 21:45:14 -0300

    show hidden files

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,13 @@ require('config.options')
 require('config.keybinds')
 require('config.lazy')
 
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+    }
+  }
+})
 vim.diagnostic.config({
     virtual_text = {
         source = "always",
