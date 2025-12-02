a31e0fb89252fa58e7c35eb1120da6432a626a9d
Rael Araujo <raellyedson1@gmail.com>
2025-12-01 21:02:46 -0300

    options, alias, guicursor

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,10 +1,21 @@
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.scrolloff = 8
-vim.opt.undofile = true
-vim.opt.mouse = 'a'
-vim.cmd('syntax on')
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.tabstop = 4           -- Tabulation with 4 spaces
+vim.opt.shiftwidth = 4        -- Indentation with 4 spaces
+vim.opt.expandtab = true      -- Replace TAB with spaces
+vim.opt.autoindent = true     -- Enable automatic indentation
+vim.opt.smarttab = true       -- Smart indentation
+vim.opt.smartindent = true    -- Smart indentation
+
+vim.opt.number = true         -- Show line numbers 
+--vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.cursorline = true     -- Highlight the current line
+vim.opt.signcolumn = "yes"    -- Always show the sign column
+vim.opt.mouse = 'a'           -- Enable mouse support
+vim.opt.undofile = true       -- Persistent undo history
+
+vim.opt.clipboard = 'unnamedplus' -- Enable system clipboard
+
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Q", "q", {})
+vim.api.nvim_create_user_command("Qa", "qa", {})
+
+vim.cmd([[ set guicursor= ]])
