e54a32cb88fee116ff03ea00d3f6296bdb636d99
Rael Araujo <raellyedson1@gmail.com>
2026-06-07 10:06:12 -0300

    set dracula theme, colros n refresh rate

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,7 +20,7 @@ return {
             "mason-org/mason.nvim",
             opts = {
                 ui = {
-                   icons = {
+                    icons = {
                         package_installed = "✓",
                         package_pending = "➜",
                         package_uninstalled = "✗",
